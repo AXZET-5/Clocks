2c4fbb06669a4f69c0c81266601d20ba99f99e62
AXZET-5 <51935353+AXZET-5@users.noreply.github.com>
2023-01-31 13:59:32 +0800

    fix

--- javascript/scripts.js
+++
@@ -1,3 +0,0 @@
-var datetime = new Date();
-console.log(datetime);
-document.getElementById("sgtTime").textContent = datetime; //it will print on html page
---
+++ scripts.js
@@ -0,0 +1,3 @@
+var datetime = new Date();
+console.log(datetime);
+ctj-Clocks.getElementById("sgtTime").textContent = datetime; //it will print on html page
