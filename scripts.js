90bb85c9f644d6dd521f6f4f0d81b993ad387747
AXZET-5 <51935353+AXZET-5@users.noreply.github.com>
2023-01-31 14:10:48 +0800

    .

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function refreshTime() {
-  const timeDisplay = document.getElementById("sgtTime");
+  const timeDisplay = document.getElementById("localTime");
   const dateString = new Date().toLocaleString();
   const formattedString = dateString.replace(", ", " - ");
   timeDisplay.textContent = formattedString;
 }
-  setInterval(refreshTime, 1000);
+  setInterval(refreshTime, 100);
